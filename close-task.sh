f4d4b0d558046681ed89d4941d36817396e9843e
Marco Luzzara <marco.luzzara@hotmail.it>
2021-04-17 10:20:49 +0200

    add tests for close-task
    added tests for a successful task closing and one to verify the
    index matches the last commit

--- close-task.sh
+++ close-task.sh
@@ -9,11 +9,21 @@ set -e
 test -f .CI_TASKID || die ".CI_TASKID does not exist. Cannot close a task without it"
 test -f .CI_NEXTRELEASE || die ".CI_NEXTRELEASE does not exist. Cannot close a task without it"
 
+[[ -n $(git diff --cached) ]] && die "commit the index changes or restore them before closing a task"
+
 taskId="$(cat .CI_TASKID)"
 nextRelease="$(cat .CI_NEXTRELEASE)"
 
 # check if master has new changes
-test "$(git ls-remote origin refs/heads/master | awk '{print $1}')" != "$(git merge-base master feature/$taskId)" && 
+test "$(git ls-remote origin refs/heads/master | awk '{print $1}')" != "$(git rev-parse master)" && 
     die "master branch has new changes. Merge/Rebase before closing the feature"
 
-#git fetch origin master
+# clean up .CI files
+git rm .CI_TASKID
+git rm .CI_NEXTRELEASE
+git commit -m "cleaned CI files"
+
+# create merge commit with no fast forward
+git checkout master
+(( $# == 1 )) && commit_description=$1 || commit_description="Merged branch feature/$taskId"
+git merge --no-ff -m "rif. #${taskId}:${nextRelease}:$commit_description" "feature/$taskId"
